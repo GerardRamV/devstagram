2709f875e94cdc5123bc6a001adde8da95c0e0ed
GerardRam <gramirez@smarto.mx>
2023-07-11 15:14:11 -0600

    first commit

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('titulo')
+
+Principal
+
+@endsection
+
+
+@section('contenido')
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae veritatis error ratione, consectetur incidunt numquam natus fugit qui at ipsum atque mollitia sapiente harum illo perferendis quidem, quibusdam magni vitae.
+@endsection
