90e5da008e0decc8214fe6f23d291e82f7d07c3f
GerardRam <gramirez@smarto.mx>
2023-07-14 10:37:20 -0600

    add register post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -11,8 +13,39 @@ class PostController extends Controller
         $this->middleware('auth');
     }
 
-    public function index() {
-        return view('dashboard');
+    public function index(User $user) {
+        return view('dashboard', [
+            'user' => $user
+        ]);
+    }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store(Request $request) {
+        $this->validate($request, [
+            'titulo' => 'required|max:255',
+            'descripcion' => 'required',
+            'imagen' => 'required',
+        ]);
+
+        Post::create([
+            'titulo' => $request->titulo,
+            'descripcion' => $request->descripcion,
+            'imagen' => $request-> imagen,
+            'user_id' => auth()->user()->id
+        ]);
+
+        // Otra forma de crear registros
+        // $post = new Post();
+        // $post->titulo = $request->titulo;
+        // $post->descripcion = $request->descripcion;
+        // $post->imagen = $request-> imagen;
+        // $post->user_id = auth()->user()->id;
+        // $post->save();
+
+        return redirect()->route('posts.index', auth()->user()->username);
     }
 }
 
